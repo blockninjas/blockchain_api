052221089691dac948808e3fc27a13d5d5ae3bd4
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2019-06-22 15:59:36 +0200

    Use blank password for tests with Travis CI

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :blockninjas_api, BlockninjasApi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "blockninjas_api_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
